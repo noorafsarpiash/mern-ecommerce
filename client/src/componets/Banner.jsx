74ed351f202499ee67a90016399191ea1e83feb9
Noor Piash <noorpiash1234@gmail.com>
2025-11-19 23:04:18 +0600

    servicesTag2

--- client/src/componets/Banner.jsx
+++ client/src/componets/Banner.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "slick-carousel/slick/slick.css";
 import Slider from "react-slick";
 import { bannerData } from '../constants';
 import Container from './Container';
+import { useNavigate } from 'react-router-dom';
+
 
 const Banner = () => {
+
+    const navigate = useNavigate();
+
+    const [dotActive, setDotActive] = useState(0);
 
     const settings = {
         dots: true,
@@ -13,6 +19,118 @@ const Banner = () => {
         slidesToShow: 1,
         slidesToScroll: 1,
         arrows: false,
+        beforeChange: (prev, next) => {
+            setDotActive(next);
+        },
+        appendDots: (dots) => (
+            <div
+                style={{
+                    position: "absolute",
+                    bottom: 30,
+                    left: "50%",
+                    transform: "translate(-50%)",
+                }}
+
+            >
+                <ul
+                    style={{
+                        margin: "0px",
+                        display: "flex",
+                        alignItems: "center",
+                        gap: "10px"
+
+                    }}
+                >
+                    {""}
+                    {dots}{""}
+
+                </ul>
+
+            </div>
+        ),
+
+        customPaging: (i) => {
+            return (
+                <div
+                    style={
+                        i === dotActive
+                            ? {
+                                width: "50px",
+                                height: "15px",
+                                backgroundColor: "#262626",
+                                cursor: "pointer",
+                                borderRadius: "20px",
+                            }
+                            : {
+                                width: "15px",
+                                height: "15px",
+                                backgroundColor: "#ffffff",
+                                cursor: "pointer",
+                                borderRadius: "50%",
+                            }
+                    }
+                />
+            );
+        },
+
+        responsive: [
+            {
+                breakpoint: 576,
+                settings: {
+                    dots: true,
+                    appendDots: (dots) => (
+                        <div
+                            style={{
+                                position: "absolute",
+                                bottom: 30,
+                                left: "50%",
+                                transform: "translate(-50%)",
+                            }}
+
+                        >
+                            <ul
+                                style={{
+                                    margin: "0px",
+                                    display: "flex",
+                                    alignItems: "center",
+                                    gap: "10px"
+
+                                }}
+                            >
+                                {""}
+                                {dots}{""}
+
+                            </ul>
+
+                        </div>
+                    ),
+                    customPaging: (i) => {
+                        return (
+                            <div
+                                style={
+                                    i === dotActive
+                                        ? {
+                                            width: "40px",
+                                            height: "10px",
+                                            backgroundColor: "#262626",
+                                            cursor: "pointer",
+                                            borderRadius: "20px",
+                                        }
+                                        : {
+                                            width: "10px",
+                                            height: "10px",
+                                            backgroundColor: "#ffffff",
+                                            cursor: "pointer",
+                                            borderRadius: "50%",
+                                        }
+                                }
+                            />
+                        );
+                    },
+                }
+            }
+        ]
+
 
     };
 
@@ -31,7 +149,7 @@ const Banner = () => {
                                     <h2 className='text-xl md:text-5xl max-w-sm  md:max-w-xl font-bold md:leading-[55px] capitalize'>{item?.title}</h2>
                                     <p className='text-xs md:text-base uppercase font-medium'>{item?.discount}</p>
                                     <p className='font-medium text-sm md:text-base'>From <span className='md:text-xl font-bold text-blue-700 md:ml-2'> ${item?.from}</span></p>
-                                    <button className='w-24 md:w-44 py-2 md:py-0 md:h-12 bg-black/80 text-white rounded-md text-xs md:text-sm uppercase font-sermibold hover:bg-black hoverEffect'>Shop Now</button>
+                                    <button onClick={() => navigate("/shop")} className='w-24 md:w-44 py-2 md:py-0 md:h-12 bg-black/80 text-white rounded-md text-xs md:text-sm uppercase font-sermibold hover:bg-black hoverEffect'>Shop Now</button>
                                 </Container>
                             </div>
                         </div>
--- client/src/componets/ServicesTag.jsx
+++ client/src/componets/ServicesTag.jsx
@@ -1,8 +1,52 @@
-import React from 'react'
+import { TbTruckDelivery } from "react-icons/tb";
+import { HiOutlineCurrencyDollar } from "react-icons/hi";
+import { BiSupport } from "react-icons/bi";
+import { MdOutlinePayment } from "react-icons/md";
+
+import Container from "./Container";
+
+export const services = [
+    {
+        title: "Free delivery",
+        subtitle: "Free shipping on all order",
+        icon: <TbTruckDelivery />,
+    },
+    {
+        title: "Return",
+        subtitle: "Contact us 24 hours a day",
+        icon: <HiOutlineCurrencyDollar />,
+    },
+    {
+        title: "Secure payment",
+        subtitle: "100% secure payment",
+        icon: <BiSupport />,
+    },
+    {
+        title: "Payments",
+        subtitle: "100% secure payment",
+        icon: <MdOutlinePayment />,
+    },
+];
+
+
 
 const ServicesTag = () => {
     return (
-        <div>ServicesTag</div>
+        <div className="bg-[#f4f4f4]">
+            <Container className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5 place-items-center md:place-items-start  " >
+                {
+                    services?.map((item, index) => (
+                        <div key={index} className="flex item-center gap-2">
+                            <span className="text-5xl text-blue-600">  {item?.icon}</span>
+                            <div>
+                                <h3 className="text-base uppercase font-bold">{item?.title}</h3>
+                                <p className="text-sm font-medium max-w-[160px] tracking-wide">{item?.subtitle}</p>
+                            </div>
+                        </div>
+                    ))
+                }
+            </Container>
+        </div>
     )
 }
 
