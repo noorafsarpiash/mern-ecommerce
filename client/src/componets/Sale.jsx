30e208b33f85d842ad7bf164ddb746f86e0a0730
Noor Piash <noorpiash1234@gmail.com>
2025-11-22 20:28:51 +0600

    home section done3

--- client/src/componets/Sale.jsx
+++ client/src/componets/Sale.jsx
@@ -25,26 +25,50 @@ const Sale = () => {
             </div>
 
 
-            <div className="w-full md:w-1/2 h-[300px] flex flex-col justify-between  md:h-full bg-gray-200 gap-0">
 
-                <div className='w-full h-[250px] md:h-[46%] border border-gray-300 rounded-md overflow-hidden relative group'>
+
+            <div className="w-full md:w-1/2 h-[300px] flex flex-col justify-between  md:h-full gap-10">
+
+                <div className='w-full h-[250px] md:justify-start md:h-[46%] border border-gray-300 rounded-md overflow-hidden relative group'>
                     <img
                         className="w-full h-full object-cover group-hover:scale-110 duration-500 ease-in-out"
+                        src={saleImgTwo}
+                        alt=""
+                    />
 
-                        src={saleImgTwo} alt="" />
-                    <div className='absolute w-full h-full top-0 left-0 bg-black/40 text-white/80 flex items-center justify-center'>
-                        <div className='flex flex-col items-center gap-2'>
+                    <div className='absolute w-full h-full top-0 left-0 bg-black/40 text-white/80 flex items-center md:items-start'>
+                        <div className='flex flex-col gap-2 p-10 justify-center'>
                             <p className='text-sm md:text-lg font-medium text-white'>10% sales ongoing on phone</p>
                             <p className='text-sm md:text-xl font-semibold'>Offers on limited time</p>
-                            <Link className='bg-white/70 text-black px-8 py-3 rounded-md hover:bg-white duration-300 font-medium' to="/shop">Shop Now</Link>
+                            <Link
+                                className='bg-white/70 text-black  w-fit  px-8 py-3 rounded-md hover:bg-white duration-300 font-medium'
+                                to="/shop"
+                            >
+                                Shop Now
+                            </Link>
                         </div>
                     </div>
                 </div>
-                <div>
-                    <img src={saleImgThree} alt="saleImgThree" />
 
+                <div className="w-full h-[250px] md:h-[46%] border border-gray-300 rounded-md overflow-hidden relative group">
+                    <img
+                        src={saleImgThree}
+                        alt="saleImgThree"
+                        className="w-full h-full object-cover group-hover:scale-110 duration-500 ease-in-out"
+                    />
+                    <div className='absolute w-full h-full top-0 left-0 bg-black/40 text-white/80 flex items-center md:items-start'>
+                        <div className='flex flex-col gap-2 p-10 justify-center'>
+                            <p className='text-sm md:text-lg font-medium text-white'>10% sales ongoing on phone</p>
+                            <p className='text-sm md:text-xl font-semibold'>Offers on limited time</p>
+                            <Link
+                                className='bg-white/70 text-black px-8 py-3 w-fit rounded-md hover:bg-white duration-300 font-medium'
+                                to="/shop"
+                            >
+                                Shop Now
+                            </Link>
+                        </div>
+                    </div>
                 </div>
-
             </div>
         </div>
     )
